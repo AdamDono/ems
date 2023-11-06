3249d00b3c318e74b8ffebb5852b5e9c9af06995
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-11-06 14:17:28 +0200

    added form select drop down

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements AfterViewInit {
+  @ViewChild ('tempButton') buttontemp: any
+
+  title = 'Dashboard';
+
+  educationOptions = [
+    'Grade 10',
+    'Grade 12',
+    'diploma',
+    'Undergraduate',
+  ];
+
+  ngAfterViewInit(): void {
+    this.buttontemp.nativeElement.click
+  }
 
   constructor() { }
 
