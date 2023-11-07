ef1442fb65731c26e4bd63d31b626d6a2010640c
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-11-07 09:49:10 +0200

    sudo instaleed json

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,21 +1,43 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements AfterViewInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
   @ViewChild ('tempButton') buttontemp: any
 
   title = 'Dashboard';
 
+  employeeform: FormGroup | undefined;
+
   educationOptions = [
-    'Grade 10',
+    'Drop Out',
     'Grade 12',
     'diploma',
     'Undergraduate',
   ];
+
+
+  constructor (private fb: FormBuilder) {
+    this.employeeform = fb.group({
+  });
+
+}
+ngOnInit(): void {
+  this.employeeform = this.fb.group({
+    firstname: this.fb.control(''),
+    lastname: this.fb.control(''),
+    birthday: this.fb.control(''),
+    gender: this.fb.control(''),
+    education: this.fb.control(''),
+    company: this.fb.control(''),
+    jobexpirience: this.fb.control(''),
+    salary: this.fb.control(''),
+  });
+}
 
   ngAfterViewInit(): void {
     this.buttontemp.nativeElement.click
@@ -23,9 +45,7 @@ export class DashboardComponent implements AfterViewInit {
 
 
   //need to learn more about this 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+
 
 }//
